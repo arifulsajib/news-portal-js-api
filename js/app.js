d8fbba759901be3d0417afee6fed8f47d758779d
arifulsajib <allaboutsajib@gmail.com>
2023-03-17 03:07:00 +0600

    major function done

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
-// Display all news on load
-fetchData("https://openapi.programming-hero.com/api/news/category/08", displayNews);
+//click handler category
+const categoriesClick = (id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/0${id}`;
+  fetchData(url, displayNews);
+
+  //   active category
+  toggleActiveClass(id, false);
+  toggleActiveClass(id, true);
+  const catName = getValue(`cat-0${id}`);
+  setValue("category-foundName", catName);
+};
+
+// Display news category on load
+fetchData("https://openapi.programming-hero.com/api/news/categories", displayCategoryNews);
+
+// Display news on load
+fetchData("https://openapi.programming-hero.com/api/news/category/01", displayNews);
--- js/utilities.js
+++ js/utilities.js
@@ -12,13 +12,143 @@ const setValue = (id, value) => {
 
 // fetch data function
 const fetchData = (url, displayData) => {
+  toggleLoading(true);
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayData(data.data));
 };
-// display data function
-const displayNews = (news) => {
-  console.log(news);
+
+// display news categories function
+const displayCategoryNews = (newsCatobj) => {
+  const newsCatArr = newsCatobj.news_category;
+
+  const newsCatContainer = document.getElementById("categoryNews-container");
+  newsCatContainer.innerHTML = "";
+
+  //   loop through each
+  for (const newsCat of newsCatArr) {
+    const url = `https://openapi.programming-hero.com/api/news/category/${newsCat.category_id}`;
+    const newsCatDiv = document.createElement("div");
+    newsCatDiv.classList.add("d-inline-block");
+    newsCatDiv.classList.add("mb-1");
+    newsCatDiv.innerHTML = `
+    <a class="category text-decoration-none text-grey mx-2 p-1 rounded" id="cat-${newsCat.category_id}" href="#" onclick="categoriesClick(${newsCat.category_id})">${newsCat.category_name}</a>
+    `;
+    newsCatContainer.appendChild(newsCatDiv);
+  }
+};
+
+// display News function
+const displayNews = (newsArr) => {
+  const newsContainer = document.getElementById("news-container");
+  newsContainer.innerHTML = "";
+
+  //   not found
+  if (newsArr.length === 0) {
+    toggleNotFound(true);
+    toggleLoading(false);
+  } else {
+    setValue("data-num", newsArr.length);
+    toggleNotFound(false);
+  }
+  //   loop through each
+  for (const news of newsArr) {
+    // console.log(news);
+    const newsDiv = document.createElement("div");
+    newsDiv.classList.add("col");
+    newsDiv.classList.add("col-md-11");
+    newsDiv.classList.add("mx-auto");
+
+    newsDiv.innerHTML = `
+    <div class="card p-2 border-0 shadow" onclick="displayDetailsModal()">
+      <div class="row g-0">
+        <div class="col-md-4 my-auto">
+          <img src="${news.thumbnail_url}" class="w-100 rounded-4" alt="..." style="max-height: 320px" />
+        </div>
+        <div class="col-md-8 my-auto">
+          <div class="card-body">
+            <h5 class="card-title fw-semibold mb-3">${news.title}</h5>
+            <p class="card-text">${news.details.slice(0, 130)}</p>
+            <p class="card-text d-none d-md-block">${news.details.slice(131, 260)}...</p>
+            <div class="d-flex justify-content-between align-items-center flex-wrap mt-4">
+              <div class="d-flex">
+                <img class="rounded-circle my-auto" src="${news.author.img}" alt="" width="35px" height="35px" />
+                <p class="my-auto ms-2">${news.author.name} <br />${news.author?.published_date?.slice(0, 10)}</p>
+              </div>
+              <p class="my-auto"><i class="fa-solid fa-eye"></i> ${news.total_view}</p>
+              ${displayRating(news.rating.number)}
+              <button class="btn d-none d-lg-block"><i class="fa-solid fa-arrow-right"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+
+    newsContainer.appendChild(newsDiv);
+    toggleLoading(false);
+  }
+};
+
+// display details model
+const displayDetailsModal = () => {
+  console.log("okay");
+};
+
+// display rating
+const displayRating = (num) => {
+  const numRounded = Math.round(num);
+  if (numRounded === 1) {
+    return `
+    <div>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-regular fa-star text-warning"></i>
+        <i class="fa-regular fa-star text-warning"></i>
+        <i class="fa-regular fa-star text-warning"></i>
+        <i class="fa-regular fa-star text-warning"></i>
+    </div>
+    `;
+  } else if (numRounded === 2) {
+    return `
+    <div>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-regular fa-star text-warning"></i>
+        <i class="fa-regular fa-star text-warning"></i>
+        <i class="fa-regular fa-star text-warning"></i>
+    </div>
+    `;
+  } else if (numRounded === 3) {
+    return `
+    <div>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-regular fa-star text-warning"></i>
+        <i class="fa-regular fa-star text-warning"></i>
+    </div>
+    `;
+  } else if (numRounded === 4) {
+    return `
+    <div>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-regular fa-star text-warning"></i>
+    </div>
+    `;
+  } else {
+    return `
+    <div>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+    </div>
+    `;
+  }
 };
 
 // toggleLoading function
@@ -32,11 +162,24 @@ const toggleLoading = (isShow) => {
 };
 
 // toggleActive function for Cat
-const toggleActiveClass = (isActive) => {
-  const loading = document.getElementById("loading");
+const toggleActiveClass = (id, isActive) => {
+  const activeEl = document.getElementById(`cat-0${id}`);
   if (isActive) {
-    loading.classList.add("activeCat");
+    activeEl.classList.add("activeCat");
   } else {
-    loading.classList.remove("activeCat");
+    const noActiveEl = document.querySelectorAll(".category");
+    noActiveEl.forEach((el) => {
+      el.classList.remove("activeCat");
+    });
+  }
+};
+
+// toggleLoading function
+const toggleNotFound = (isShow) => {
+  const notFound = document.getElementById("not-found");
+  if (isShow) {
+    notFound.classList.remove("d-none");
+  } else {
+    notFound.classList.add("d-none");
   }
 };
